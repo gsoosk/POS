78a8f7a0f7c36b475f8fecf1df78f12595b0fc5f
amirhossein ahmadi <amirhmi@amirhosseins-MacBook-Pro.local>
2019-01-11 11:56:00 +0330

    shm test add

--- shmTest.c
+++ shmTest.c
@@ -44,22 +44,10 @@ void simple_shm_test()
     int pid = fork();
     if(pid > 0)
     {
-<<<<<<< HEAD
-        shm_open(1,(char **)&counter);
+        shm_open(1, 1, 0);
         counter ->cnt = 10;
         printf(1, "counter value in parent is : %d\n", (counter->cnt));
         releasesleep_syscalls();
-=======
-       
-        shm_open(1,1, 0);
-        void *  tmp;
-        tmp = (void*) shm_attach(1);
-        counter =  (struct shm_cnt *) tmp ;
-        counter -> cnt = 10;
-        printf(1, "counter in p %x\n", (counter));
-        printf(1, "counter in parent %d\n", (counter->cnt));
-        printf(1, "b\n", (counter->cnt));
->>>>>>> origin/l
         wait();
         shm_close(1);
     }
@@ -82,7 +70,7 @@ void shm_write_flag_test() {
     int pid = fork();
     if(pid > 0)
     {
-        shm_open(1,(char **)&counter);
+        shm_open(1, 1, 0);
         counter ->cnt = 10;
         printf(1, "counter value in parent is : %d\n", (counter->cnt));
         releasesleep_syscalls();
@@ -95,7 +83,7 @@ void shm_write_flag_test() {
         counter =  (struct shm_cnt *) shm_attach(1);
         counter->cnt++;
         printf(1, "counter value increased in child\n");
-        printf(1, "counter value in parent is : %d\n", (counter->cnt));
+        printf(1, "counter value in child is : %d\n", (counter->cnt));
         shm_close(1);
         releasesleep_syscalls();
     }
